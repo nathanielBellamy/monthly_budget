3e750f803a96e4534981d955bd16a72614ca8d0f
nathanielBellamy <nbschieber@gmail.com>
2023-01-05 08:34:21 -0800

    add time and rand crates

--- src/lib/money/amount.rs
+++ src/lib/money/amount.rs
@@ -1,4 +1,4 @@
-// use rand::Rng;
+use rand::Rng;
 
 pub struct Amount {
     // use to over/under estimate
@@ -8,29 +8,39 @@ pub struct Amount {
 }
 
 impl Amount {
-    pub fn randomize() -> usize {
-        // let res = rand::thread_rng().gen_range(self.low..self.high);
-        // res
-        20
+    pub fn randomize(&self) -> usize {
+        let mut _low: usize = 0;
+        let mut _high: usize = 1;
+        match self.low {
+            Some(low) => _low = low,
+            _ => (),
+        }
+        match self.high {
+            Some(high) => _high = high,
+            _ => (),
+        }
+        let res = rand::thread_rng().gen_range(_low.._high);
+        res
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn randomize_returns_random_number_between_low_and_high() {
+        let amount = test_amount();
+        let randomized = amount.randomize();
+        assert!(randomized < amount.high.unwrap());
+        assert!(randomized > amount.low.unwrap());
+    }
+
     fn test_amount() -> Amount {
         Amount {
             low: Some(10),
             high: Some(100),
             standard: 50,
         }
-    }
-
-    #[test]
-    fn randomize_returns_random_number_between_low_and_high() {
-        let amount = test_amount();
-        let randomized = Some(20);
-        assert!(randomized < amount.high);
-        assert!(randomized > amount.low);
     }
 }

a389d1e30bf929e6dbd0393fc5f36501a86f7abf
nathanielBellamy <nbschieber@gmail.com>
2023-02-08 19:39:17 -0800

    init recurrence struct

---
+++ src/schema/recurrance.rs
@@ -0,0 +1,39 @@
+use crate::traits::csv_record::CsvRecord;
+use crate::traits::csv_store::CsvStore;
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Recurrence {
+    pub id: Option<usize>,
+    pub frequency: Every,
+}
+
+pub enum Every {
+    Days(u8),   // Every::Days(2) = every other day
+    Weeks(u8),  // Every::Weeks(2) = every other week
+    Months(u8), // Every::Months(6) = twice a year
+    Years(u8),  // Every::Years(10) = once per decade
+}
+
+#[allow(unused)]
+pub type RecurrenceStore = BTreeMap<usize, Recurrence>;
+
+impl CsvRecord<Recurrence> for Recurrence {
+    fn id(&self) -> Option<usize> {
+        self.id
+    }
+
+    fn set_id(&mut self, new_id: usize) -> Option<usize> {
+        self.id = Some(new_id);
+        self.id
+    }
+
+    fn clone_record(&self) -> Recurrence {
+        Recurrence {
+            id: self.id,
+            content: self.content.clone(),
+        }
+    }
+}
+impl CsvStore<Recurrence> for Recurrence {}

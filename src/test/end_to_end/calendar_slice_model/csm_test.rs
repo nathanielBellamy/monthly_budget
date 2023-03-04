7e84b6cd109c871b66dfed73fdde48fc1996b13f
nathanielBellamy <nbschieber@gmail.com>
2023-03-03 20:05:28 -0800

    fmt

--- src/test/end_to_end/calendar_slice_model/csm_test.rs
+++ src/test/end_to_end/calendar_slice_model/csm_test.rs
@@ -7,11 +7,11 @@ impl CsmTest {
     #[allow(unused)] // used in tests below
     pub fn run() -> u8 {
         let cli = Cli::new(
-            CsmTest::format_path("init/"), // input
-            CsmTest::format_path("reports/"),      // output
-            CsmTest::format_path("events"),        // payment_events
-            "2023-03".to_string(),                 // start
-            "2023-06".to_string(),                 // end
+            CsmTest::format_path("init/"),    // input
+            CsmTest::format_path("reports/"), // output
+            CsmTest::format_path("events"),   // payment_events
+            "2023-03".to_string(),            // start
+            "2023-06".to_string(),            // end
         );
         println!("CSMTEST RUN");
         match CalendarSliceModel::run_cli(cli) {
@@ -29,9 +29,9 @@ impl CsmTest {
 #[cfg(test)]
 mod calendar_slice_model_e2e {
     use super::*;
-    use std::fs;
     use crate::storage::store::Store;
     use rust_decimal::Decimal;
+    use std::fs;
     use std::sync::Once;
 
     // call test once
@@ -60,14 +60,14 @@ mod calendar_slice_model_e2e {
         let mut store = Store::new();
         store.init(Some(STORE_INIT.to_string())).unwrap();
         assert_eq!(37, store.account_balances.len());
-       
+
         let mid_balance = store.account_balances[&12];
         assert_eq!(2, mid_balance.account_id);
-        assert_eq!(Decimal::new(8500,0), mid_balance.amount);
-        
+        assert_eq!(Decimal::new(8500, 0), mid_balance.amount);
+
         let final_balance = store.account_balances[&37];
         assert_eq!(2, final_balance.account_id);
-        assert_eq!(Decimal::new(31_900,0), final_balance.amount);
+        assert_eq!(Decimal::new(31_900, 0), final_balance.amount);
     }
 
     #[test]
@@ -75,10 +75,13 @@ mod calendar_slice_model_e2e {
         run_test();
         let mut store = Store::new();
         store.init(Some(STORE_INIT.to_string())).unwrap();
-        
+
         assert_eq!(24, store.payments.len());
         let final_payment = store.payments[&8];
-        assert_eq!(Decimal::new(200,0), final_payment.amount(&mut store.amounts).unwrap().standard);
+        assert_eq!(
+            Decimal::new(200, 0),
+            final_payment.amount(&mut store.amounts).unwrap().standard
+        );
     }
 
     #[test]
@@ -86,12 +89,15 @@ mod calendar_slice_model_e2e {
         run_test();
         let mut store = Store::new();
         store.init(Some(STORE_INIT.to_string())).unwrap();
-        
+
         assert_eq!(12, store.payments_received.len());
         let final_payment_received = store.payments_received[&3];
         assert_eq!(
-            Decimal::new(5000,0), 
-            final_payment_received.amount(&mut store.amounts).unwrap().standard
+            Decimal::new(5000, 0),
+            final_payment_received
+                .amount(&mut store.amounts)
+                .unwrap()
+                .standard
         );
     }
 
@@ -100,7 +106,7 @@ mod calendar_slice_model_e2e {
         run_test();
         let mut store = Store::new();
         store.init(Some(STORE_INIT.to_string())).unwrap();
-        
+
         assert_eq!(2, store.accounts.len());
     }
 
@@ -109,7 +115,7 @@ mod calendar_slice_model_e2e {
         run_test();
         let mut store = Store::new();
         store.init(Some(STORE_INIT.to_string())).unwrap();
-        
+
         assert_eq!(4, store.expenses.len());
         assert_eq!("Groceries".to_string(), store.expenses[&1].name);
         assert_eq!("Mortgage".to_string(), store.expenses[&2].name);
@@ -122,7 +128,7 @@ mod calendar_slice_model_e2e {
         run_test();
         let mut store = Store::new();
         store.init(Some(STORE_INIT.to_string())).unwrap();
-        
+
         assert_eq!(2, store.incomes.len());
         assert_eq!("Space Man".to_string(), store.incomes[&1].name);
         assert_eq!("Cowboy".to_string(), store.incomes[&2].name);
